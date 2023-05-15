4a8613216d5465bb7840c94e6de9a24425382b92
Derek Meduri <derekmeduri@gmail.com>
2023-05-15 09:21:46 -0400

    cloning starter code

---
+++ script.js
@@ -0,0 +1,78 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function getPasswordOptions() {
+  // Variable to store length of password from user input
+  var length = parseInt(
+    prompt("How many characters would you like your password to contain?"),
+    10
+  );
+
+  // need more code below
+
+  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (Number.isNaN(length)) {
+    alert("Password length must be provided as a number");
+    return null;
+  }
+
+  // Variable to store boolean regarding the inclusion of special characters
+  var hasSpecialCharacters = confirm(
+    "Click OK to confirm including special characters."
+  );
+
+  // Object to store user input
+  var passwordOptions = {
+    length: length,
+    // add more properties and values here
+  };
+
+  return passwordOptions;
+}
+
+// Function for getting a random element from an array(all instances of arr will be replaced by an ACTUAL VALUE when we do our callback.)
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  var options = getPasswordOptions();
+  // Variable to store password as it's being concatenated
+  var result = [];
+
+  // Array to store types of characters to include in password
+
+  //copy arrays created in other vs code file here.
+  var possibleCharacters = [];
+
+  // Array to contain one of each type of chosen character to ensure each will be used
+  var guaranteedCharacters = [];
+
+  // Check if an options object exists, if not exit the function
+  if (!options) return null;
+
+  // Conditional statement that adds array of special characters into array of possible characters based on user input
+  // Push new random special character to guaranteedCharacters
+  if (options.hasSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+
+  // Transform the result into a string and pass into writePassword
+  return result.join("");
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
